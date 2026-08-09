982e155d6cadfb11f6f3fafc9822417c7677b99d
Angel2222522 <142777531+Angel2222522@users.noreply.github.com>
2026-08-09 14:48:45 +0300

    Fix content for app/src/test/java/dev/devicecontrolcenter/FoundationTest.kt

--- app/src/test/java/dev/devicecontrolcenter/FoundationTest.kt
+++ app/src/test/java/dev/devicecontrolcenter/FoundationTest.kt
@@ -1,1 +1,11 @@
-sed: can't read : No such file or directory
+package dev.devicecontrolcenter
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FoundationTest {
+    @Test
+    fun arithmeticSanityCheck() {
+        assertEquals(4, 2 + 2)
+    }
+}
