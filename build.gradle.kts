a3f2a74671527aa6f83f19587987f8945b5f7108
Angel2222522 <142777531+Angel2222522@users.noreply.github.com>
2026-08-09 14:48:32 +0300

    Fix content for build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,5 @@
-sed: can't read : No such file or directory
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+}
